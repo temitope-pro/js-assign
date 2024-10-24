4de3cb9d3353b5681adda3c47198bc35631d2dd9
temitope <omodeletemitope828@gmail.com>
2024-10-24 14:36:41 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let aboutmyself = {
+    firstname: 'temitope',
+    lastname: 'tomiwa',
+    origin: 'akinyele',
+    town: 'ibadan',
+    state: 'oyo state',
+    country: 'nigeria',
+    age: 16,
+    gender: 'male',
+    profession: 'undergraduate',
+    color: 'red',
+    food: 'rice and beans',
+
+}
+alert(` 
+My name is ${aboutmyself.firstname} ${aboutmyself.lastname} 
+i live in ${aboutmyself.origin} ${aboutmyself.town} ${aboutmyself.state} ${aboutmyself.country},
+i am ${aboutmyself.age} years old,
+my best food is ${aboutmyself.food} and also
+my best color is ${aboutmyself.color} i am an ${aboutmyself.profession}`)
